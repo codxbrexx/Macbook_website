66a88b8f810662bc6d4f309615a91f6bd7a4d88a
codxbrexx <mozammilali2018@gmail.com>
2025-10-20 04:16:15 +0530

    performance

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@
 import './App.css'
 import { Hero } from './components/Hero'
 import { Navbar } from './components/Navbar'
+import Performance from './components/performance'
 import Productviewer from './components/Productviewer'
+import Showcase from './components/Showcase'
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
       <Navbar />
       <Hero />
       <Productviewer />
+      <Showcase />
+      <Performance />
     </>
   )
 }
