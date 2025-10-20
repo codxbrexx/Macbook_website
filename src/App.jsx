ae09eab32a79fb595c98978cefcf86305a603b99
codxbrexx <mozammilali2018@gmail.com>
2025-10-20 15:52:41 +0530

    debug01

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
+import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
+import ProductViewer from "./components/ProductViewer.jsx";
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/all";
+import Showcase from "./components/Showcase.jsx";
+import Performance from "./components/Performance.jsx";
+import Features from "./components/Features.jsx";
+import Highlights from "./components/Highlights.jsx";
+import Footer from "./components/Footer.jsx";
 
-import { Features } from 'tailwindcss'
-import './App.css'
-import { Hero } from './components/Hero'
-import { Navbar } from './components/Navbar'
-import Performance from './components/performance'
-import Productviewer from './components/Productviewer'
-import Showcase from './components/Showcase'
-import Highlights from './components/Highlights'
+gsap.registerPlugin(ScrollTrigger)
 
-function App() {
-
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <Productviewer />
-      <Showcase />
-      <Performance />
-      <Features />
-      <Highlights />
-    </>
-  )
+const App = () => {
+    return (
+        <main>
+            <NavBar />
+            <Hero />
+            <ProductViewer />
+            <Showcase />
+            <Performance />
+            <Features />
+            <Highlights />
+            <Footer />
+        </main>
+    )
 }
 
 export default App
